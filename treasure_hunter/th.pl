1fc38a7406f1e0ddb41f7afcebc0e9c58feadf1d
bomber34 <kunstkr1tik@web.de>
2019-09-15 01:53:05 +0200

    removed redundant REST var of nth1

--- treasure_hunter/th.pl
+++ treasure_hunter/th.pl
@@ -4,7 +4,7 @@ game(1,L,3,4):-
 		2,2,3,4,
 		3,3,1,1
 		].
-		
+
 game(2,L,11,22):-
 	L = [
 	1,3,4,4,4,3,4,2,4,3,4,1,4,1,2,4,3,2,4,4,4,2,
@@ -20,6 +20,21 @@ game(2,L,11,22):-
 	2,2,3,2,3,4,2,2,2,2,1,1,3,2,1,1,1,2,3,4,4,1
 	].
 
+game(3, L, 11, 22):-
+	L = [
+	4,3,4,3,3,1,3,2,4,3,4,4,2,1,4,2,3,1,3,4,4,1,
+	4,3,2,3,2,4,4,1,1,3,3,4,4,3,4,1,1,3,2,4,4,3,
+	1,1,2,4,2,2,3,1,3,2,1,2,4,3,2,3,2,2,4,3,3,2,
+	2,2,1,1,1,4,3,4,1,2,1,2,2,1,1,3,1,1,2,4,2,1,
+	4,4,1,4,2,4,1,1,3,4,4,2,2,4,4,3,4,3,1,4,4,1,
+	1,4,3,2,4,4,3,4,3,1,4,3,1,4,2,2,2,3,3,1,3,3,
+	3,2,3,1,3,2,3,3,2,1,2,1,3,2,3,1,4,2,4,3,4,1,
+	1,3,2,2,1,2,2,1,2,3,4,1,4,1,3,1,1,1,2,2,2,2,
+	2,4,1,4,2,4,1,3,4,3,4,2,4,1,3,4,4,2,2,3,4,2,
+	1,1,4,3,1,4,2,1,2,2,1,1,4,2,3,3,4,1,2,1,1,4,
+	3,3,2,2,3,3,4,2,3,3,4,1,2,2,1,1,3,3,4,1,2,3
+	].
+
 start(Game):-
 	game(Game,State,Height,Width),
 	%display_map(State,Height,Width),
@@ -27,8 +42,12 @@ start(Game):-
 	solve(State,Height,Width,StartTime, [State]).
 
 solve(State,Height,Width,StartTime, Steps):-
-	(all_cleared(State); get_time(EndTime), Diff is EndTime - StartTime, Diff > 300.0), display_all_maps(Steps,Height,Width), !.
-	
+	(
+        all_cleared(State)
+	%get_time(EndTime), Diff is EndTime - StartTime, Diff > 240.0
+        ),
+	display_all_maps(Steps,Height,Width, 5), !.
+
 solve(State,Height,Width,StartTime, Steps):-
 	Size is Height * Width,
 	length(WorkState,Size),
@@ -38,24 +57,28 @@ solve(State,Height,Width,StartTime, Steps):-
 	update_neighbours(State,Size,Width,1,WorkState),
 	finish_update(WorkState,NextState),
 	%display_map(NextState,Height,Width),
-	append(Steps, [NextState], MoreSteps),
+	append(Steps, [WorkState], MoreSteps),
 	solve(NextState,Height,Width, StartTime,MoreSteps).
-	
+
 all_cleared([]).
-all_cleared([o|T]):- all_cleared(T).	
-	
+all_cleared([o|T]):- all_cleared(T).
+
 finish_update([],[]).
 finish_update([x|T],[o|T2]):- finish_update(T,T2).
-finish_update([H|T],[H|T2]):- H \= x, finish_update(T,T2).	
+finish_update([H|T],[H|T2]):- H \= x, finish_update(T,T2).
 
-display_all_maps([],_,_).
-display_all_maps([Map|T],Height,Width):-
-	display_map(Map,Height,Width), display_all_maps(T,Height,Width).
+display_all_maps([],_,_, _).
+display_all_maps([Map|T],Height,Width, 0):-
+	get_single_char(_),!,
+	display_all_maps([Map|T],Height,Width, 5).
+display_all_maps([Map|T],Height,Width, Nums):-
+	Nums2 is Nums - 1,
+	display_map(Map,Height,Width), display_all_maps(T,Height,Width,Nums2).
 
 display_map(Map,Height,Width):-
 	display_map(Map,Width,Height,Width).
 
-display_map([],_,_,0):- nl,nl,!.	
+display_map([],_,_,0):- nl,nl,!.
 display_map(Map,Width,H,0):-
 	nl,
 	NextH is H-1,
@@ -67,31 +90,31 @@ display_map([Field|T],Width,H,W):-
 
 update_neighbours(_, Size,_, Position, _):- Position > Size, !.
 update_neighbours(State, Size,Width,Position,NextState):-
-	nth1(Position,NextState, Spot, _),
+	nth1(Position,NextState, Spot),
 	var(Spot),
-	nth1(Position,State, Value, _),
+	nth1(Position,State, Value),
 	((neighbour_got_removed(Size,Width,Position,NextState), Value \= o) ->
-	
+
 		NextVal is Value + 1,
 		update_val(NextVal,Spot),!;
 		Spot = Value
 	),
 	NextPos is Position + 1,
 	update_neighbours(State, Size, Width, NextPos, NextState), !.
-	
+
 update_neighbours(State, Size,Width,Position,NextState):-
-	nth1(Position,NextState, Spot, _),
+	nth1(Position,NextState, Spot),
 	nonvar(Spot),
 	NextPos is Position + 1,
 	update_neighbours(State, Size,Width, NextPos, NextState).
-	
+
 update_val(Val,Spot):-
 		Val < 5,
 		Spot = Val.
 update_val(5,Spot):-
 	Spot = 1.
-	
-neighbour_got_removed(Size,Width,Position,NextState):- 
+
+neighbour_got_removed(Size,Width,Position,NextState):-
 	EPos is Position+1,
 	WPos is Position-1,
 	NPos is Position-Width,
@@ -102,47 +125,48 @@ neighbour_got_removed(Size,Width,Position,NextState):-
 	neighbour_got_removed(Size,NPos,NextState),!;
 	neighbour_got_removed(Size,SPos,NextState)
 	).
-	
+
 neighbour_got_removed(Size,Pos,NextState):-
 	Pos =< Size,
 	Pos > 0,
-	nth1(Pos,NextState,Spot,_),
+	nth1(Pos,NextState,Spot),
 	nonvar(Spot),
 	Spot = x.
 
+%true if this position does not resemble value west and north of it
 is_not_prev(_,1,_).
 is_not_prev(State,Position,Width):-
 	Position > Width,
 	WPos is Position - 1,
 	NPos is Position - Width,
-	nth1(Position,State,Val,_),
-	not(nth1(WPos,State,Val,_)),
-	not(nth1(NPos,State,Val,_)),!.
+	nth1(Position,State,Val),
+	not(nth1(WPos,State,Val)),
+	not(nth1(NPos,State,Val)),!.
 is_not_prev(State,Position,_):-
 	Position > 1,
 	WPos is Position - 1,
-	nth1(Position,State,Val,_),
-	not(nth1(WPos,State,Val,_)),!.
+	nth1(Position,State,Val),
+	not(nth1(WPos,State,Val)),!.
 
 get_coordinates(State,Height, Width, X, Y):-
 	between(1,Width,X),
 	between(1,Height, Y),
 	Position is ((Y-1) * Width) + X,
 	is_not_prev(State,Position,Width),
-	nth1(Position, State, Value, _),
+	nth1(Position, State, Value),
 	Value \= x,
 	Value \= o,
 	(
-		X < Width, XEast is X + 1, NextPos is ((Y-1) * Width) + XEast, nth1(NextPos, State, Value,_);
-		X > 1, XWest is X - 1, NextPos is ((Y-1) * Width) + XWest, nth1(NextPos, State, Value,_);
-		Y > 1, YNorth is Y - 1, NextPos is ((YNorth-1) * Width) + X, nth1(NextPos, State, Value,_);
-		Y < Height, YSouth is Y + 1, NextPos is ((YSouth-1) * Width) + X, nth1(NextPos, State, Value,_)
+		X < Width, XEast is X + 1, NextPos is ((Y-1) * Width) + XEast, nth1(NextPos, State, Value);
+		X > 1, XWest is X - 1, NextPos is ((Y-1) * Width) + XWest, nth1(NextPos, State, Value);
+		Y > 1, YNorth is Y - 1, NextPos is ((YNorth-1) * Width) + X, nth1(NextPos, State, Value);
+		Y < Height, YSouth is Y + 1, NextPos is ((YSouth-1) * Width) + X, nth1(NextPos, State, Value)
 	).
 
 remove_fields(State,Height, Width, X,Y,NextState):-
 	Position is ((Y-1) * Width) + X,
-	nth1(Position, State, Value, _),
-	nth1(Position, NextState, x, _),
+	nth1(Position, State, Value),
+	nth1(Position, NextState, x),
 	remove_neighbours(State, Height, Width, Value,X,Y,NextState).
 
 remove_neighbours(State, Height, Width, Value,X,Y,NextState):-
@@ -159,8 +183,8 @@ remove_neighbour(State, Height, Width, Value,X,Y,NextState):-
 	X > 0, X =< Width,
 	Y > 0, Y =< Height,
 	Position is ((Y-1) * Width) + X,
-	nth1(Position, State, Value, _),
-	nth1(Position, NextState, FreeSpot, _),
+	nth1(Position, State, Value),
+	nth1(Position, NextState, FreeSpot),
 	var(FreeSpot),
 	FreeSpot = x,
 	remove_neighbours(State, Height, Width, Value,X,Y,NextState).
@@ -169,11 +193,11 @@ remove_neighbour(_, _, _, _,_,_,_):- true.
 
 has_valid_position(State,Width,Height,X,Y):-
 	Position is ((Y-1) * Width) + X,
-	nth1(Position, State, Value, _),
+	nth1(Position, State, Value),
 	(
-		X>1, XEast is X - 1, NextPos is ((Y-1) * Width) + XEast, nth1(NextPos, State, Value,_),!;
-		X < Width, XWest is X + 1, NextPos is ((Y-1) * Width) + XWest, nth1(NextPos, State, Value,_),!;
-		Y > 1, YNorth is Y - 1, NextPos is ((YNorth-1) * Width) + X, nth1(NextPos, State, Value,_),!;
-		Y < Height, YSouth is Y + 1, NextPos is ((YSouth-1) * Width) + X, nth1(NextPos, State, Value,_),!
+		X>1, XEast is X - 1, NextPos is ((Y-1) * Width) + XEast, nth1(NextPos, State, Value),!;
+		X < Width, XWest is X + 1, NextPos is ((Y-1) * Width) + XWest, nth1(NextPos, State, Value),!;
+		Y > 1, YNorth is Y - 1, NextPos is ((YNorth-1) * Width) + X, nth1(NextPos, State, Value),!;
+		Y < Height, YSouth is Y + 1, NextPos is ((YSouth-1) * Width) + X, nth1(NextPos, State, Value),!
 	).
-	
+
